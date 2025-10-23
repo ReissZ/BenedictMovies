1cd89229249d96af37d2bd072e1a221445c482b0
Reiss <reiss@theapplabb.com>
2025-10-23 12:57:37 -0400

    Initial Commit

---
+++ BenedictMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BenedictMovies
+//
+//  Created by Reiss Zurbyk on 2025-10-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BenedictMoviesTests/BenedictMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  BenedictMoviesTests.swift
+//  BenedictMoviesTests
+//
+//  Created by Reiss Zurbyk on 2025-10-23.
+//
+
+import Testing
+@testable import BenedictMovies
+
+struct BenedictMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
